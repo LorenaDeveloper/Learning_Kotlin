faacf530a5c478d1b2fa7ed45db0b897c5cbe5d8
LorenaDeveloper <developerlorena@yahoo.com>
2021-12-30 15:09:05 +0000

    using objectMapper in tests

--- product/src/test/kotlin/com/KotlinProject/product/ProductApplicationTests.kt
+++ product/src/test/kotlin/com/KotlinProject/product/ProductApplicationTests.kt
@@ -1,5 +1,10 @@
 package com.KotlinProject.product
 
+import com.KotlinProject.product.Model.Product
+import com.KotlinProject.product.Service.ProductService
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -14,11 +19,23 @@ class ProductApplicationTests {
 
 	@Autowired
 	private lateinit var mockMvc: MockMvc
+	@Autowired
+	private lateinit var objectMapper: ObjectMapper
+	@Autowired
+	private lateinit var productService: ProductService
 
 	@Test
 	fun findAllTest() {
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product/"))
+		var productsFromService:List<Product> = productService.findAll()
+		val json = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product/"))
 			.andExpect(MockMvcResultMatchers.status().isOk)
+			.andReturn().response.contentAsString
+
+		var products:List<Product> = objectMapper.readValue(json)
+
+		assertThat(products.size).isEqualTo(productsFromService.size)
+		assertThat(products.get(0).name).isEqualTo(productsFromService.get(0).name)
+		assertThat(products.get(0).price).isEqualTo(productsFromService.get(0).price)
 	}
 
 }
