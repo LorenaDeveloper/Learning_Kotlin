ef64b306499315601d704fcf96c76c6a7c93ca1f
LorenaDeveloper <developerlorena@yahoo.com>
2021-12-30 14:19:03 +0000

    cleaning comments and finishing section2

--- product/src/main/kotlin/com/KotlinProject/product/Controller/ProductController.kt
+++ product/src/main/kotlin/com/KotlinProject/product/Controller/ProductController.kt
@@ -5,42 +5,10 @@ import com.KotlinProject.product.Service.ProductService
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-//to use abstract class
+//using abstract class
 @RestController
 @RequestMapping("/api/v1/product")
 class ProductController(productService: ProductService):BasicController<Product, String>(productService)
 
-
-//@RestController
-//@RequestMapping("/api/v1/product")
-//class ProductController(@Autowired private val productService: ProductService) { // dependency injection by constructor
-//
-//    @GetMapping("/")
-//    fun findAll() = productService.findAll()
-//
-//    @GetMapping("/{id}")
-//    fun findById(@PathVariable id:String) = productService.findById(id)
-//
-//    @PostMapping
-//    fun addProduct(@RequestBody product: Product ) = productService.save(product)
-//
-//    @PutMapping
-//    fun updateProduct(@RequestBody product: Product ) = productService.update(product)
-//
-//    @DeleteMapping("/{id}")
-//    fun deleteById(@PathVariable id:String) = productService.deleteById(id)
-//}
-
-//another way to do it
-//@RestController
-//@RequestMapping("/api/v1/product")
-//class ProductController {
-//
-//    @Autowired
-//    private lateinit var productService: ProductService // 'lateinit' indicates that it'll be initialized later
-//
-//    @GetMapping("/get")
-//    fun findAll() = productService.findAll()
-//}
 
 
--- product/src/main/kotlin/com/KotlinProject/product/Service/ProductService.kt
+++ product/src/main/kotlin/com/KotlinProject/product/Service/ProductService.kt
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Service
 
 @Service
 class ProductService:BasicCrud<Product, String> {       // implements interface BasicCrud
-    //this way it create inmutable set
-    //private val products:Set<Product> =  setOf(Product("Apple", 22.2), Product(price = 33.3, name = "Banana"))
-
     //to create mutable set
     private val products:MutableSet<Product> =  mutableSetOf(Product("Apple", 22.2), Product(price = 33.3, name = "Banana"))
 
@@ -17,20 +14,14 @@ class ProductService:BasicCrud<Product, String> {       // implements interface 
 
     override fun findById(id: String): Product? {
         return this.products.find { it.name == id } //lambda function. find the product whose name equals string 'id'.
-                                                    // uses 'it' as iterator
+                                                    // uses 'it' as iterator; another way -> { product -> product.name == id }
     }
-
-//    //another way of using this iterator
-//    override fun findById(id: String): Product? {
-//        return this.products.find { product -> product.name == id }
-//    }
 
     override fun save(t: Product): Boolean {
         return this.products.add(t)
     }
 
     override fun update(t: Product): Boolean {
-        //there isn't update sentence, so remove and add
         return this.deleteById(t.name) && this.products.add(t)
     }
 
