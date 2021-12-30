27ccd9222a18fbed941378e8a3c97844f723f1d5
LorenaDeveloper <developerlorena@yahoo.com>
2021-12-30 10:08:01 +0000

    upgrading controller and service and adding product

--- product/src/main/kotlin/com/KotlinProject/product/Controller/ProductController.kt
+++ product/src/main/kotlin/com/KotlinProject/product/Controller/ProductController.kt
@@ -1,13 +1,29 @@
 package com.KotlinProject.product.Controller
 
 import com.KotlinProject.product.Service.ProductService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/product")
-class ProductController(private val productService: ProductService) {
+class ProductController(@Autowired private val productService: ProductService) { // dependency injection by constructor
+
     @GetMapping("/get")
     fun findAll() = productService.findAll()
 }
+
+//another way to do it
+//@RestController
+//@RequestMapping("/api/v1/product")
+//class ProductController {
+//
+//    @Autowired
+//    private lateinit var productService: ProductService // 'lateinit' indicates that it'll be initialized later
+//
+//    @GetMapping("/get")
+//    fun findAll() = productService.findAll()
+//}
+
+
