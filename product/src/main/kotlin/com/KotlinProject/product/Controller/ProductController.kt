fd33eb8a851359bbd1c7b1585bdeff3b74fa155a
LorenaDeveloper <developerlorena@yahoo.com>
2021-12-30 13:27:04 +0000

    adding pathVariable to controller

--- product/src/main/kotlin/com/KotlinProject/product/Controller/ProductController.kt
+++ product/src/main/kotlin/com/KotlinProject/product/Controller/ProductController.kt
@@ -3,6 +3,7 @@ package com.KotlinProject.product.Controller
 import com.KotlinProject.product.Service.ProductService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/product")
 class ProductController(@Autowired private val productService: ProductService) { // dependency injection by constructor
 
-    @GetMapping("/get")
+    @GetMapping("/")
     fun findAll() = productService.findAll()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id:String) = productService.findById(id)
 }
 
 //another way to do it
