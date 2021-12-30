45f231ae572ec840f3311e09a5b764472e0ef168
LorenaDeveloper <developerlorena@yahoo.com>
2021-12-30 14:44:15 +0000

    first test

--- product/src/test/kotlin/com/KotlinProject/product/ProductApplicationTests.kt
+++ product/src/test/kotlin/com/KotlinProject/product/ProductApplicationTests.kt
@@ -1,13 +1,24 @@
 package com.KotlinProject.product
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductApplicationTests {
 
+	@Autowired
+	private lateinit var mockMvc: MockMvc
+
 	@Test
-	fun contextLoads() {
+	fun findAllTest() {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product/"))
+			.andExpect(MockMvcResultMatchers.status().isOk)
 	}
 
 }
