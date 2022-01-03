80053858d258102c65115fbb457cdc1af7f8e177
LorenaDeveloper <developerlorena@yahoo.com>
2022-01-03 13:32:20 +0000

    set MockMvc manually

--- product/src/test/kotlin/com/KotlinProject/product/ProductApplicationTests.kt
+++ product/src/test/kotlin/com/KotlinProject/product/ProductApplicationTests.kt
@@ -7,18 +7,27 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class ProductApplicationTests {
 
 	@Autowired
-	private lateinit var mockMvc: MockMvc
+	private lateinit var webApplicationContext : WebApplicationContext
+
+	// initializes MockMvc on request and print every step of each test
+	private val mockMvc: MockMvc by lazy {
+		MockMvcBuilders.webAppContextSetup(webApplicationContext)
+			.alwaysDo<DefaultMockMvcBuilder>(MockMvcResultHandlers.print()).build()
+	}
+
 	@Autowired
 	private lateinit var objectMapper: ObjectMapper
 	@Autowired
