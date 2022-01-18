aca7889623a6f18f1acb5bd21413189c3fc53e5f
LorenaDeveloper <developerlorena@yahoo.com>
2022-01-18 16:38:33 +0000

    adding error handler

--- product/src/main/kotlin/com/KotlinProject/product/Controller/ProductController.kt
+++ product/src/main/kotlin/com/KotlinProject/product/Controller/ProductController.kt
@@ -2,13 +2,20 @@ package com.KotlinProject.product.Controller
 
 import com.KotlinProject.product.Model.Product
 import com.KotlinProject.product.Service.ProductService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.FileNotFoundException
 
 //using abstract class
 @RestController
 @RequestMapping("/api/v1/product")
-class ProductController(productService: ProductService):BasicController<Product, String>(productService)
+class ProductController(productService: ProductService):BasicController<Product, String>(productService){
+    @GetMapping("/test")
+    fun fileNotFoundTest(){
+        throw FileNotFoundException("just test")
+    }
+}
 
 
 
